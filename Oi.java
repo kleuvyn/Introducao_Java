8483af309226cfc066772c40e919c2922a00fb77
kleuvyn <kleuvyn@hotmail.com>
2023-06-03 02:00:09 -0300

    inicio

--- Oi.java
+++ Oi.java
@@ -1,6 +1,8 @@
 public class Oi {
     public static void main(String[] args) {
 
-        System.out.println("OLá Mundo!");
+        System.out.println("Olá, Mundo!");
+        System.out.println("Java não é um monstro como falam!");
+
     }
 }
