da070bfe85833b1d2c2f6e2deb74d44ea09b10a8
kleuvyn <kleuvyn@hotmail.com>
2023-06-13 10:45:04 -0300

    aprendendo a sintaxe

---
+++ VarIdade.java
@@ -0,0 +1,11 @@
+public class VarIdade {
+    public static void main(String[] args) {
+        System.out.println("Olá novo teste!");
+
+        int idade;
+                idade = 28;
+
+        System.out.println("A idade é " + idade + " anos");
+
+    }
+}
