ca8ea3ef5631e65f1a631a18c65ca1631cfe0b5d
kleuvyn <kleuvyn@hotmail.com>
2023-07-12 10:55:02 -0300

    up

--- Bank Herdado/src/ControleBonificacao.java
+++ Bank Herdado/src/ControleBonificacao.java
@@ -7,4 +7,19 @@ public class ControleBonificacao {
         double bon = g.getBonificacao();
         this.soma = this.soma + bon;
     }
+    public void registra(Funcionario fun){
+
+        double bon = fun.getBonificacao();
+        this.soma = this.soma + bon;
+    }
+
+    public void registra(EditorVideo editorv){
+
+        double bon = editorv.getBonificacao();
+        this.soma = this.soma + bon;
+    }
+
+    public double getSoma() {
+        return soma;
+    }
 }
--- Bank Herdado/src/TestReferencias.java
+++ Bank Herdado/src/TestReferencias.java
@@ -5,16 +5,29 @@ public class TestReferencias {
         Gerente gr01 = new Gerente();
         gr01.setNome("Miguel Silva");
         gr01.setCpf(987456123);
-        gr01.setSalario(6500.00);
+        gr01.setSalario(5000.00);
         gr01.getBonificacao();
 
+        Funcionario fun = new Funcionario();
+        fun.setSalario(2000.00);
 
-        System.out.println("Nome: " + gr01.getNome() + "\nCPF: " + gr01.getCpf() + "\nSalario: " + gr01.getSalario() + "\nBonificação: " + gr01.getBonificacao());
+        EditorVideo editorv = new EditorVideo();
+        fun.setSalario(2500.00);
+
+        ControleBonificacao controle = new ControleBonificacao();
+        controle.registra(gr01);
+        controle.registra(fun);
+        controle.registra(editorv);
+
+
+        System.out.println("Nome: " + gr01.getNome() + "\nCPF: " + gr01.getCpf() + "\nSalario: " + gr01.getSalario() + "\nBonificação: " +  controle.getSoma());
 
         gr01.setSenha(0306);
         boolean autenticou = gr01.autentica(0306);
 
         System.out.println("Senha: " + autenticou);
-}
+
+        System.out.println(controle.getSoma());
+    }
 
 }
