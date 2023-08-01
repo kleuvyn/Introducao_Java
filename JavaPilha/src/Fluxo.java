b91f11225e593bac7bf6c3c6240ed4f7e28f3518
kleuvyn <kleuvyn@hotmail.com>
2023-08-01 09:47:38 -0300

    up

--- JavaPilha/src/Fluxo.java
+++ JavaPilha/src/Fluxo.java
@@ -1,31 +1,30 @@
 public class Fluxo {
+
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo01();
+        try {
+            metodo1();
+        } catch(ArithmeticException | NullPointerException ex) {
+            String msg = ex.getMessage();
+            System.out.println("Exception " + msg);
+            ex.printStackTrace();
+        }
         System.out.println("Fim do main");
-
     }
-    private static  void metodo01() {
-        System.out.println("Ini do metodo01");
-        metodo02();
-        System.out.println("Fim do metodo01");
 
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
     }
-    private  static void metodo02() {
-        System.out.println("Ini do metodo02\n");
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
-            try {
-                int a = i / 0;
-                Conta c = null;
-                c.deposita();
-            } catch(ArithmeticException | NullPointerException ex) {
-                String msg = ex.getMessage();
-                System.out.println("Exception " + msg);
-                ex.printStackTrace();
-            }
+            Conta c = null;
+            c.deposita();
         }
-        System.out.println("Fim do metodo02");
+        System.out.println("Fim do metodo2");
     }
-
 }
