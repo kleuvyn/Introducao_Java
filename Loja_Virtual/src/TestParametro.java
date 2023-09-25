45e74a40d7b0fc9184b6b60f270085cdc246c7d6
kleuvyn <kleuvyn@hotmail.com>
2023-09-25 10:43:42 -0300

    SQL injection

--- Loja_Virtual/src/TestParametro.java
+++ Loja_Virtual/src/TestParametro.java
@@ -1,35 +1,52 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.*;
 
 public class TestParametro {
 
     public static void main(String[] args) throws SQLException {
 
-        String PRODUTO  = "026856";
-        String NOME = "Cola";
-        Float PRECO_LISTA = 5.33f;
         Conector estabelecer = new Conector();
         Connection connection = estabelecer.conexao();
+        connection.setAutoCommit(false);
 
-        PreparedStatement stm =
-                connection.prepareStatement ("INSERT INTO  tbPRODUTO (PRODUTO, NOME, PRECO_LISTA) VALUES ( ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 
+        try (PreparedStatement stm =
+                     connection.prepareStatement("INSERT INTO  tbPRODUTO (PRODUTO, NOME, PRECO_LISTA) VALUES ( ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+        ){
+            adicionarVariavel("0996876", "Pepsi 1.5l", 9.99f, stm);
+            adicionarVariavel("03684584", "Refri Limao 500Ml", 3.99f, stm);
+
+            connection.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("ROLLBACK EXECUTADO");
+            connection.rollback();
+        }
+    }
+
+
+    private static void adicionarVariavel(String PRODUTO, String NOME, Float PRECO_LISTA, PreparedStatement stm) throws SQLException {
         stm.setString(1, PRODUTO);
         stm.setString(2, NOME);
         stm.setFloat(3, PRECO_LISTA);
 
-        stm.execute();
-
-
-        ResultSet rst = stm.getGeneratedKeys();
-        while (rst.next()) {
-            String produto = rst.getString(1);
-            System.out.println("O produto criado foi: " + produto);
-
+        if(PRODUTO.equals("Sucos")) {
+            throw new RuntimeException("Não foi possível adicionar o produto");
         }
 
+        stm.execute();
+
+        try(ResultSet rst = stm.getGeneratedKeys()){
+            while(rst.next()) {
+                String produto = rst.getString(1);
+                System.out.println("O id criado foi: " + PRODUTO);
+            }
+        }
     }
 }
+
+
+
