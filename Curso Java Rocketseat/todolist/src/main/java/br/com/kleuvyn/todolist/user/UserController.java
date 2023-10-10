d9bfcf299e077ad929e925cc113cf4d79966e1fb
kleuvyn <kleuvyn@hotmail.com>
2023-10-10 09:42:35 -0300

    Aula 02 - Integração com Banco de Dados Nessa aula você vai aprender mais sobre desenvolvimento em Java, Spring Framework e interações com banco de dados e seguir na construção do seu projeto usando as ferramentas mais modernas.
    Daniele Leão

--- Curso Java Rocketseat/todolist/src/main/java/br/com/kleuvyn/todolist/user/UserController.java
+++ Curso Java Rocketseat/todolist/src/main/java/br/com/kleuvyn/todolist/user/UserController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @PostMapping("/")
     public void create (@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+        System.out.println(userModel.getUsername());
 
     }
 
--- Curso Java Rocketseat/todolist/src/main/java/br/com/kleuvyn/todolist/user/UserModel.java
+++ Curso Java Rocketseat/todolist/src/main/java/br/com/kleuvyn/todolist/user/UserModel.java
@@ -2,6 +2,32 @@ package br.com.kleuvyn.todolist.user;
 
 public class UserModel {
     private String username;
-    public String name;
-    public String password;
+    private String name;
+    private  String password;
+
+    public void setUsername(String username ){
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
